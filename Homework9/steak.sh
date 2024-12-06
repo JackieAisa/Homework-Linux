8c5da8a26774c0e3ee1a3cffb4f37334d13905f7
EC2 Default User <ec2-user@ip-172-31-88-88.ec2.internal>
2024-12-06 16:24:55 +0000

    Homework9, part2

---
+++ Homework9/steak.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "What is the meet's internal temperature?: " temp
+
+if [ $temp -gt 120 ] && [ $temp -le 130 ]
+then
+	echo "Rare"
+
+
+elif [ $temp -gt 131 ] && [ $temp -le 140 ]
+then
+	echo "Medium rare"
+
+
+elif [ $temp -gt 141 ] && [ $temp -le 150 ]
+then
+	echo "Medium"
+
+
+elif [ $temp -gt 151 ] && [ $temp -le 160 ]
+then
+	echo "Medium wll"
+
+
+elif [ $temp -gt 161 ] && [ $temp -le 170 ]
+then
+	echo "Well done"
+
+fi	
