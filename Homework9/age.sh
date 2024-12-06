13387aa36de39b078bd72106f84bcf4bd4a7a113
EC2 Default User <ec2-user@ip-172-31-88-88.ec2.internal>
2024-12-06 16:22:52 +0000

    Homework9

---
+++ Homework9/age.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+read -p "What year were you born? " year
+
+echo You are $((2024-$year)) years old.
+echo You were $((2020-$year)) years old in 2020.
+echo You will be $((2030-$year)) years old in 2030.
+
+
